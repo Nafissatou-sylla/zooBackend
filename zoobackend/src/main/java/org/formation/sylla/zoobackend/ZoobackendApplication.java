496d1af249a2abed3fd7ec5e400192da128fc3a2
Sylla <finasylla2020@gmail.com>
2023-02-17 09:30:21 +0100

    projet spring

---
+++ zoobackend/src/main/java/org/formation/sylla/zoobackend/ZoobackendApplication.java
@@ -0,0 +1,13 @@
+package org.formation.sylla.zoobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoobackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoobackendApplication.class, args);
+	}
+
+}
---
+++ zoobackend/src/test/java/org/formation/sylla/zoobackend/ZoobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.formation.sylla.zoobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
